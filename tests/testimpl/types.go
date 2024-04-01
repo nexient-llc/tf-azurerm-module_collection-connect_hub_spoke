b82621e30361065732ccc09a7f1c8397b0282a42
nexient-ajoshi <97111743+nexient-ajoshi@users.noreply.github.com>
2024-04-01 11:13:52 -0400

    Update tests/testimpl/types.go
    Co-authored-by: Chris Taylor <132399041+chris11-taylor-nttd@users.noreply.github.com>

--- tests/testimpl/types.go
+++ tests/testimpl/types.go
@@ -4,5 +4,4 @@ import "github.com/nexient-llc/lcaf-component-terratest-common/types"
 
 type ThisTFModuleConfig struct {
 	types.GenericTFModuleConfig
-	// Empty: there are no settings for the skeleton module.
 }
